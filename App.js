988bd12c5c1d81481ed49813a23c92561a257397
minkeun <minguni4@gmail.com>
2023-04-19 02:48:56 +0900

    how to delete db clearly...T..T

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import Navigator from "./src/navigation";
-import { Amplify } from "aws-amplify";
+import { Amplify, Auth, API, graphqlOperation } from "aws-amplify";
 import { withAuthenticator } from "aws-amplify-react-native";
 import awsconfig from "./src/aws-exports";
+import { useEffect } from "react";
+import { getUser } from "./src/graphql/queries";
+import { createUser, updateUser, deleteUser } from "./src/graphql/mutations";
+
 Amplify.configure({ ...awsconfig, Analytics: { disabled: true } });
+
 function App() {
+  useEffect(() => {
+    const syncUser = async () => {
+      //get Auth user
+      const authUser = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
+      console.log(authUser);
+
+      //query the database using Auth user id(sub)
+      const userData = await API.graphql(
+        graphqlOperation(getUser, { id: authUser.attributes.sub })
+      );
+
+      console.log(userData);
+
+      const aaaaa = await API.graphql(
+        graphqlOperation(deleteUser, { input: { id: authUser.attributes.sub } })
+      );
+      console.log(aaaaa);
+
+      if (userData.data.getUser) {
+        console.log("user already exists in DB");
+        return;
+      }
+      //if there is no users in db, create one
+
+      const newUser = {
+        id: authUser.attributes.sub,
+        name: authUser.attributes.email,
+        status: "moooooo yo!",
+      };
+      const newUserResponse = await API.graphql(
+        graphqlOperation(createUser, { input: newUser })
+      );
+    };
+    syncUser();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Navigator />
